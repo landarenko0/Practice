aad4de6314d5187f00c40ef096ced5ab19974cca
landarenko0 <landarenko_nikita@mail.ru>
2022-07-06 23:59:49 +0500

    If state is 'integer' and class of current lexeme is 'letter', return 0. Also added adding current lexeme to lexeme_chain if lexeme is not empty

--- lex.py
+++ lex.py
@@ -100,25 +100,20 @@ def lexical(data):
         elif (data[i][1] == "letter" or data[i][1] == "number") and state == "identifier":
             current_lexeme += data[i][0]
         elif data[i][1] == "letter" and state == "integer":
-            return False
+            return 0
         elif data[i][1] == "number" and state == "integer":
             current_lexeme += data[i][0]
 
+    if not isEmpty(current_lexeme):
+        lexeme_chain.append([current_lexeme, lexeme_class])
     return lexeme_chain
 
 
-lexeme = lexical([[' ', 'space'], [' ', 'space'], ['r', 'letter'], ['e', 'letter'], ['p', 'letter'], ['e', 'letter'],
-                  ['a', 'letter'], ['t', 'letter'], ['u', 'letter'], ['n', 'letter'], ['t', 'letter'], ['i', 'letter'],
-                  ['l', 'letter'], [' ', 'space'], ['r', 'letter'], ['e', 'letter'], ['p', 'letter'], ['e', 'letter'],
-                  ['a', 'letter'], ['t', 'letter'], [' ', 'space'], ['u', 'letter'], ['u', 'letter'], [' ', 'space'],
-                  [' ', 'space'], [' ', 'space'], [' ', 'space'], ['u', 'letter'], ['n', 'letter'], ['t', 'letter'],
-                  ['i', 'letter'], ['l', 'letter'], [' ', 'space'], [';', 'semicolon'], [' ', 'space'], ['<', 'less'],
-                  ['>', 'more'], ['=', 'equal'], [' ', 'space'], ['$', 'dollar_sign'], [' ', 'space'], [' ', 'space'],
-                  ['a', 'letter'], ['b', 'letter'], [' ', 'space'], [' ', 'space'], ['b', 'letter'], ['c', 'letter'],
-                  [' ', 'space'], ['9', 'number'], ['2', 'number'], ['6', 'number'], [' ', 'space'], ['9', 'number'],
-                  ['1', 'number'], ['3', 'number'], [' ', 'space'], ['r', 'letter'], [' ', 'space'], ['m', 'letter'],
-                  ['y', 'letter'], ['f', 'letter'], ['u', 'letter'], ['n', 'letter'], ['c', 'letter'],
-                  ['(', 'bracket1'], ['+', 'plus-minus'], ['-', 'plus-minus'], ['*', 'multiplication'], [' ', 'space'],
-                  ['d', 'letter'], ['i', 'letter'], ['v', 'letter'], [' ', 'space'], ['m', 'letter'], ['o', 'letter'],
-                  ['d', 'letter'], [')', 'bracket2'], [' ', 'space'], [' ', 'space']])
+lexeme = lexical([['r', 'letter'], ['e', 'letter'], ['p', 'letter'], ['e', 'letter'], ['a', 'letter'], ['t', 'letter'],
+                  ['t', 'letter'], [' ', 'space'], ['f', 'letter'], ['u', 'letter'], ['n', 'letter'], ['c', 'letter'],
+                  ['(', 'bracket1'], ['$', 'dollar_sign'], ['A', 'letter'], ['B', 'letter'], ['C', 'letter'],
+                  [' ', 'space'], ['d', 'letter'], ['i', 'letter'], ['v', 'letter'], [' ', 'space'], ['m', 'letter'],
+                  ['i', 'letter'], ['n', 'letter'], [')', 'bracket2'], [' ', 'space'], ['u', 'letter'], ['n', 'letter'],
+                  ['t', 'letter'], ['i', 'letter'], ['l', 'letter'], [' ', 'space'], ['B', 'letter'], ['<', 'less'],
+                  ['1', 'number'], ['0', 'number'], [';', 'semicolon']])
 print(lexeme)
