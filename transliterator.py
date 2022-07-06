0b368e9d05eb8aa7a1e33bb3af3204ae46fa8338
landarenko0 <landarenko_nikita@mail.ru>
2022-07-06 23:50:43 +0500

    If there are unexpected symbol, the chain will be rejected

--- transliterator.py
+++ transliterator.py
@@ -2,64 +2,63 @@ def transliteration(chain):
     lexeme_chain = []
 
     for i in range(len(chain)):
+        symbol = chain[i]
         symbol_class = []
-        if str.isalpha(chain[i]):
+        if str.isalpha(symbol):
             class_of_symbol = "letter"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif str.isnumeric(chain[i]):
+        elif str.isnumeric(symbol):
             class_of_symbol = "number"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == ";":
+        elif symbol == ";":
             class_of_symbol = "semicolon"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] in "+-":
+        elif symbol in "+-":
             class_of_symbol = "plus-minus"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] in "*":
+        elif symbol in "*":
             class_of_symbol = "multiplication"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == " ":
+        elif symbol == " ":
             class_of_symbol = "space"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == "(":
+        elif symbol == "(":
             class_of_symbol = "bracket1"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == ")":
+        elif symbol == ")":
             class_of_symbol = "bracket2"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == "$":
+        elif symbol == "$":
             class_of_symbol = "dollar_sign"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == ">":
+        elif symbol == ">":
             class_of_symbol = "more"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == "<":
+        elif symbol == "<":
             class_of_symbol = "less"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
-        elif chain[i] == "=":
+        elif symbol == "=":
             class_of_symbol = "equal"
-            symbol_class.append(chain[i])
+            symbol_class.append(symbol)
             symbol_class.append(class_of_symbol)
         else:
-            class_of_symbol = "error"
-            symbol_class.append(chain[i])
-            symbol_class.append(class_of_symbol)
+            return 0
 
         lexeme_chain.append(symbol_class)
 
     return lexeme_chain
 
 
-lexeme = transliteration("  repeatuntil repeat uu    until ; <>= $  ab  bc 926 913 r myfunc(+-* div mod)  ")
+lexeme = transliteration("repeat func($ABC div min) until B<10;")
 print(lexeme)
