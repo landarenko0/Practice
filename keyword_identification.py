a495a1f86b0cd89e3afcfd11327442630f1161d7
landarenko0 <landarenko_nikita@mail.ru>
2022-07-10 13:39:43 +0500

    Added linear search

--- keyword_identification.py
+++ keyword_identification.py
@@ -1,8 +1,16 @@
-def identification(data):
-    keywords = ["and", "array", "begin", "case", "const", "div", "do", "downto", "else", "end", "file", "for",
-                "function", "goto", "if", "in", "label", "mod", "nil", "not", "of", "or", "packed", "procedure",
-                "program", "record", "repeat", "set", "then", "to", "type", "until", "var", "while", "with"]
+keywords = ["and", "array", "begin", "case", "const", "div", "do", "downto", "else", "end", "file", "for",
+            "function", "goto", "if", "in", "label", "mod", "nil", "not", "of", "or", "packed", "procedure",
+            "program", "record", "repeat", "set", "then", "to", "type", "until", "var", "while", "with"]
 
+
+def linear_search(word):
+    for i in range(len(keywords)):
+        if word.lower() == keywords[i]:
+            return 1
+    return 0
+
+
+def identification(data):
     for i in range(len(data)):
         if data[i][0] == "repeat" or data[i][0] == "Repeat" or data[i][0] == "REPEAT":
             data[i][1] = "keyword_repeat"
@@ -12,8 +20,9 @@ def identification(data):
             data[i][1] = "keyword_div"
         elif data[i][0] == "mod" or data[i][0] == "Mod" or data[i][0] == "MOD":
             data[i][1] = "keyword_mod"
-        elif data[i][0] in keywords:
-            return 0
+        else:
+            if linear_search(data[i][0]) == 1:
+                return 0
 
     return data
 
